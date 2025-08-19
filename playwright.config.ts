6e8bd1740b79c6189230d28954feb1280d3c27fa
Mac <komalrana@MacBook-Air.local>
2025-08-19 20:51:13 +0530

    Running tests.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,16 +4,10 @@ const isCI = !!process.env.CI;
 
 export default defineConfig({
   testDir: './src/tests',
-  projects: [
-    {
-      name: 'chrome',
-      use: {
-        channel: 'chrome',
-        headless: isCI, // ✅ Headless only in CI
-        screenshot: 'only-on-failure',
-        video: 'retain-on-failure',
-      },
-    },
-  ],
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  use: {
+    headless: isCI ? true : false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
 });
